a3b2ef4c904d5c260f4c37ffa73179678d87e1a2
RaccoonCode96 <waawoo13@gmail.com>
2021-04-01 23:05:03 +0900

    Setup (React, Firebase), routes, router, components, .env(secure API)

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import AppRouter from './Router';
+
+
+function App() {
+  return (
+    <AppRouter />
+  );
+}
+
+export default App;
---
+++ src/components/Router.js
@@ -0,0 +1,30 @@
+import React, {useState} from 'react';
+import {HashRouter as Router, Route, Switch} from "react-router-dom";
+import Auth from '../routes/Auth'; 
+import Home from '../routes/Home';
+
+const AppRouter = () => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    return (
+        <Router>
+            <Switch>
+                {isLoggedIn ? (
+                    <>
+                    <Route exact path="/">
+                        <Home />
+                    </Route>
+                </> 
+                ) : (
+                    <Route exact path="/">
+                        <Auth />
+                    </Route>
+                )}
+            </Switch>
+        </Router>
+    )
+}
+
+export default AppRouter; 
+
+
+// <> : Fragment 인데 부모요소가 없을 때  많은 요소들을 render 하고 싶을 때씀
