8f940b7367f089a17d7825df5b5dc89444032493
RaccoonCode96 <waawoo13@gmail.com>
2021-04-02 22:51:57 +0900

    firebase web api, Absolute Imports, check login, Firebase Authentication, Auth Form

--- README.md
+++ README.md
@@ -8,4 +8,6 @@
 ---
 
 
-### 2021-04-01 [Setup (React, Firebase), routes, router, components, .env(secure API)] / tistory blog :  https://goforit.tistory.com/76
+2021-04-01 [Setup (React, Firebase), routes, router, components, .env(secure API)] / tistory blog :  https://goforit.tistory.com/76
+
+2021-04-02 [firebase web api, Absolute Imports, check login, Firebase Authentication, Auth Form] / tistory blog : https://goforit.tistory.com/77  
--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import AppRouter from './Router';
+import React, {useState} from 'react';
+import AppRouter from 'components/Router';
+import { authService } from "fBase";
 
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser); 
   return (
-    <AppRouter />
+    <>
+    <AppRouter isLoggedIn={isLoggedIn}/>
+    <footer>&copy; {new Date().getFullYear()}  Rwitter </footer>
+    </>
   );
 }
 
