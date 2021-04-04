765c1dfdf7231d4d0e4267dddbcf3f1ac5eb62ba
RaccoonCode96 <waawoo13@gmail.com>
2021-04-04 23:45:30 +0900

    check user and link login or make a new Account

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,25 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import AppRouter from 'components/Router';
 import { authService } from "fBase";
 
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser); 
+  const [init, setInit] = useState(false); // 처음에는 false이고 나중에 사용자 인식이 모두 끝났을 때 true를 통해 해당 화면을 render
+  const [isLoggedIn, setIsLoggedIn] = useState(false); 
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      if(user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, [])
+  
   return (
     <>
-    <AppRouter isLoggedIn={isLoggedIn}/>
+    {init ? <AppRouter isLoggedIn={isLoggedIn}/> : "Initializing..." }
     <footer>&copy; {new Date().getFullYear()}  Rwitter </footer>
     </>
   );
